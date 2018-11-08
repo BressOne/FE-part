5b4f96a4498af7a553bb96b11c3d317e2ddc80fc
Artyom Mordvinov <1992bress@gmail.com>
2018-11-08 14:42:07 +0300

    prettyfied

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 
 function loginFetch() {
     let payload = {
-            "login-username": document.getElementById('login-username').value,
-            "login-password": document.getElementById('login-password').value,
-        };
+        "login-username": document.getElementById('login-username').value,
+        "login-password": document.getElementById('login-password').value,
+    };
     console.log(payload);
     fetch('http://localhost:3000/login', {
         method: 'post',
@@ -14,24 +14,24 @@ function loginFetch() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(payload),
-        }).then(function(response) {
-            document.getElementById('responseLogin').innerText = response;
-        }).catch(function(err) {
-            document.getElementById('responseLogin').innerText = err;
-        });
-    };
+    }).then(function (response) {
+        document.getElementById('responseLogin').innerText = response;
+    }).catch(function (err) {
+        document.getElementById('responseLogin').innerText = err;
+    });
+};
 
 class Login extends Component {
-    render () {
+    render() {
         return (
-        <div>
-            <div id="responseLogin"></div>
-                        <p>Create account</p>
-                        <p><input type="text" id="login-username" name="login-username"></input></p>
-                        <p><input type="text" id="login-password" name="login-password"></input></p>
-                        <p><button id="login-button" onClick = {loginFetch}>Login</button></p>
-            
-        </div> )
+            <div>
+                <div id="responseLogin"></div>
+                <p>Create account</p>
+                <p><input type="text" id="login-username" name="login-username"></input></p>
+                <p><input type="text" id="login-password" name="login-password"></input></p>
+                <p><button id="login-button" onClick={loginFetch}>Login</button></p>
+
+            </div>)
     };
 };
 
