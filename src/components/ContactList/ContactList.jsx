e1b78c77000429afaef04fd1120b29bdfe8b8f36
Artyom Mordvinov <1992bress@gmail.com>
2018-12-16 18:25:51 +0300

    got contact list done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,12 +4,17 @@ import Person from "../Person/Person.jsx";
 class SearchList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      contactList: ""
+    };
 
     this.getContactList = this.getContactList.bind(this);
   }
-
+  componentDidMount() {
+    this.getContactList();
+  }
   getContactList() {
+    const thisClosure = this;
     fetch("http://localhost:3000/getContacts", {
       method: "get",
       headers: {
@@ -24,18 +29,20 @@ class SearchList extends Component {
         const array = Object.keys(list).map(key => {
           return [list[key]];
         });
-        return array.map(uname => (
-          <Person name={uname} handleAddContact={this.handleAddContact} />
-        ));
-      })
 
+        return array.map(uname => <Person name={uname} />);
+      })
+      .then(array => {
+        thisClosure.setState({ contactList: array });
+        console.log(thisClosure.contactList);
+      })
       .catch(err => {
         console.log(err);
       });
   }
 
   render() {
-    return <menu className="list-friends">{this.getContactList()}</menu>;
+    return <menu className="list-friends">{this.state.contactList}</menu>;
   }
 }
 
