2386407ca1dc5a8a3ba68d0cb85c5cdf2d823877
Artyom Mordvinov <1992bress@gmail.com>
2018-12-27 14:27:39 +0300

    discarded Context for my mentor check

--- src/components/ConversationHeader/ConversationHeader.jsx
+++ src/components/ConversationHeader/ConversationHeader.jsx
@@ -5,23 +5,23 @@ const MyContext = React.createContext();
 class ConversationHeader extends Component {
   render() {
     return (
-      <MyContext.Consumer>
-        <div className="top">
-          <div className="avatar">
-            <img width="50" height="50" src="" alt="" />
-          </div>
-          <div className="info">
-            <div className="name">
-              123
-              {/* {context => {
+      // <MyContext.Consumer>
+      <div className="top">
+        <div className="avatar">
+          <img width="50" height="50" src="" alt="" />
+        </div>
+        <div className="info">
+          <div className="name">
+            123
+            {/* {context => {
                 return context.selectedUser;
               }} */}
-            </div>
-            <div className="count">already 1 902 messages</div>
           </div>
-          <i className="fa fa-star" />
+          <div className="count">already 1 902 messages</div>
         </div>
-      </MyContext.Consumer>
+        <i className="fa fa-star" />
+      </div>
+      // </MyContext.Consumer>
     );
   }
 }
